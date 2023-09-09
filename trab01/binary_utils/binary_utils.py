d28f6ee001b6da36304f5cbfb0d473575cfe921b
Gustavo <gunaldoni@gmail.com>
2023-09-09 10:33:48 -0300

    Adição da função de checagem de diferença entre aquivos binários

--- trab01/binary_utils/binary_utils.py
+++ trab01/binary_utils/binary_utils.py
@@ -1,6 +1,9 @@
 import sys
 import os
 
+DEFAULT_PATH = 'DEFAULT_PATH'
+DEFAULT_WRITE_MODE = 'wb'
+FILE_SIZE = 0
 
 class BinaryUtils:
     def _generate_default_file_path(self, file_path: str):
@@ -15,31 +18,45 @@ class BinaryUtils:
         result = f'{result}_safecopy.{file_extension}'
 
         return result
+    
+    def _get_correct_byte(self, byte: bytes) -> bytes:
+        correct_byte = 0xFF & byte[0]
+        correct_byte = correct_byte.to_bytes(1, sys.byteorder)
 
-    def safe_copy(self, file_path: str, resulting_file_path: str = 'DEFAULT', start: int = 0, end: int = 0):
+        return correct_byte
+    
+    def safe_copy_part(self, file_path: str, start: int, end: int, resulting_file_path: str = DEFAULT_PATH, write_mode: str = DEFAULT_WRITE_MODE):
+        """
+        Copies a file bit to bit from start to end indexes.
+        """
+
         file_size = os.path.getsize(file_path)
 
-        if start < 0 or start > end:
-            return False
+        if start < 0:
+            raise IndexError("The start index must be a positive integer.")
 
-        if end < start or end >= file_size:
-            return False
+        if start > end:
+            raise IndexError(
+                "The start index cannot be greater than the end index.")
 
-        if resulting_file_path == 'DEFAULT':
+        if end >= file_size:
+            raise IndexError("The end index must be less than que file size.")
+
+        if resulting_file_path == DEFAULT_PATH:
             resulting_file_path = self._generate_default_file_path(file_path)
 
-        if end == 0:
+        if end == FILE_SIZE:
             end = file_size - 1
 
-        with open(file_path, mode='rb') as initial_file, open(resulting_file_path, mode='wb') as resulting_file:
+        with open(file_path, mode='rb') as initial_file, open(resulting_file_path, mode=write_mode) as resulting_file:
+            initial_file.seek(start, 0)
+
             character = initial_file.read(1)
-            current = 0
+            current = start
 
             while character:
                 if current >= start and current <= end:
-                    correct_character = 0xff & character[0]
-                    correct_character = correct_character.to_bytes(
-                        1, sys.byteorder)
+                    correct_character = self._get_correct_byte(character)
 
                     resulting_file.write(correct_character)
 
@@ -47,6 +64,13 @@ class BinaryUtils:
                 current += 1
 
         return True
+
+    def safe_copy(self, file_path: str, resulting_file_path: str = DEFAULT_PATH) -> bool:
+        """
+        Copies an entire file bit to bit.
+        """
+
+        return self.safe_copy_part(file_path, 0, FILE_SIZE, resulting_file_path)
 
     def _write_beggining(self, file_path: str, text: bytes) -> bool:
         file_size = os.path.getsize(file_path)
@@ -78,7 +102,7 @@ class BinaryUtils:
                     1, sys.byteorder)
 
                 file.write(correct_character)
-        
+
         return True
 
     def _write_end(self, file_path: str, text: bytes) -> bool:
@@ -93,24 +117,100 @@ class BinaryUtils:
                     1, sys.byteorder)
 
                 file.write(correct_character)
-        
+
         return True
 
     def write(self, file_path: str, text: bytes, position: int) -> bool:
+        """
+        Writes to a file the entire binary 
+        text on the given position. Can append text
+        to the beggining, using negative position, 
+        or to the end of the file.
+        """
         file_size = os.path.getsize(file_path)
         text_size = len(text)
 
         if position < -text_size:
-            return False
+            raise IndexError('Your text cannot fill the entire beggining of the file.')
 
         if position < 0 and abs(position) == text_size:
             self._write_beggining(file_path, text)
             return True
-        
+
         if position >= 0 and position < file_size:
             self._write_middle(file_path, text, position)
             return True
-        
+
         if position >= file_size:
             self._write_end(file_path, text)
             return True
+        
+    def _diff_same_size(self, file_path1: str, file_path2: str) -> bytes:
+        diff = bytearray()
+
+        with open(file_path1, mode='rb') as file1, open(file_path2, mode='rb') as file2:
+            character1 = file1.read(1)
+            character2 = file2.read(1)
+
+            while character1 and character2:
+                if character1 != character2:
+                    correct_character = self._get_correct_byte(character2)
+                    diff.append(ord(correct_character))
+                
+                character1 = file1.read(1)
+                character2 = file2.read(1)
+
+        return bytes(diff)
+
+    def _diff_different_size(self, file_path1: str, file_path2: str) -> bytes:
+        diff = bytearray()
+
+        file1_size = os.path.getsize(file_path1)
+        file2_size = os.path.getsize(file_path2)
+
+        if file1_size > file2_size:
+            with open(file_path1, mode='rb') as file1, open(file_path2, mode='rb') as file2:
+                character1 = file1.read(1)
+                character2 = file2.read(1)
+
+                while character2:
+                    if character1 != character2:
+                        correct_character = self._get_correct_byte(character2)
+                        diff.append(ord(correct_character))
+
+                    character1 = file1.read(1)
+                    character2 = file2.read(1)
+ 
+        else:
+            with open(file_path1, mode='rb') as file1, open(file_path2, mode='rb') as file2:
+                character1 = file1.read(1)
+                character2 = file2.read(1)
+
+                while character1:
+                    if character1 != character2:
+                        correct_character = self._get_correct_byte(character2)
+                        diff.append(ord(correct_character))
+
+                    character1 = file1.read(1)
+                    character2 = file2.read(1)
+                
+                while character2:
+                    correct_character = self._get_correct_byte(character2)
+                    diff.append(ord(correct_character))
+
+                    character2 = file2.read(1)
+        
+        return bytes(diff)
+
+    def diff(self, file_path1: str, file_path2: str) -> bytes:
+        """
+        Returns the bytes difference of the second file when compared
+        to the first file.
+        """
+        file1_size = os.path.getsize(file_path1)
+        file2_size = os.path.getsize(file_path2)
+
+        if file1_size == file2_size:
+            return self._diff_same_size(file_path1, file_path2)
+
+        return self._diff_different_size(file_path1, file_path2)
--- trab01/main.py
+++ trab01/main.py
@@ -6,12 +6,15 @@ if __name__ == '__main__':
     binary_utils = BinaryUtils()
 
     binary_utils.safe_copy('./image01.jpeg')
-    binary_utils.safe_copy('./file01.pdf')
-    binary_utils.safe_copy('./text01.txt')
+    # binary_utils.safe_copy('./file01.pdf')
+    # binary_utils.safe_copy('./text01.txt')
 
-    text = b'## Entregar 1kg de droga na rua X ##'
+    text = b'## Entregar 1kg de droga na Rua X ##'
     start = os.path.getsize('./image01_safecopy.jpeg')
     end = start + len(text) - 1
 
     binary_utils.write('./image01_safecopy.jpeg', text, start)
-    binary_utils.safe_copy('./image01_safecopy.jpeg', './texto_recuperado.txt', start, end)
+
+    print(binary_utils.diff('./image01_safecopy.jpeg', './image01.jpeg'))
+
+    # binary_utils.safe_copy('./image01_safecopy.jpeg', './texto_recuperado.txt', start, end)
